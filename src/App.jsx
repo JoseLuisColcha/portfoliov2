2d63cacf0a585e29bbe46d4cf3fea384a6c1549a
JoseLuisColcha <jose.colcha@outlook.es>
2025-08-29 10:45:19 -0500

    add main assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   return (
     <>
-      <div>App</div>
+      <div className="font-free-script text-8xl">App</div>
     </>
   )
 }
