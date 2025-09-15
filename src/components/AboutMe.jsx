dad4a9b6be74d3081b1ade058d7f5ae730c0569b
JoseLuisColcha <jose.colcha@outlook.es>
2025-09-14 21:47:35 -0500

    update about me component

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -15,9 +15,9 @@ export const AboutMe = () => {
         Soy un apasionado por la tecnología y el diseño, graduado en Desarrollo
         de Software en la EPN, tras mis estudios, comencé mi camino como
         freelance, donde aprendí a gestionar proyectos de principio a fin y a
-        trabajar de forma autónoma.Me enfoqué en el Frontend y en el diseño
+        trabajar de forma autónoma. Me enfoqué en el Frontend y en el diseño
         UI/UX, ya que disfruto crear interfaces intuitivas que generen una
-        experiencia agradable al usuario.Si bien manejo Backend y desarrollo
+        experiencia agradable al usuario. Si bien manejo Backend y desarrollo
         full stack, me identifico más con el Frontend, porque me permite unir mi
         lado técnico con mi lado creativo.
       </p>
