945f42ffe1aa9aa5b72d83fb5fb31683ba0d044d
JoseLuisColcha <jose.colcha@outlook.es>
2025-09-20 21:27:11 -0500

    change icon in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -49,7 +49,7 @@ export const Footer = () => {
           </div>
 
           <div className="flex flex-row gap-1 items-center text-brand text-[15px] ">
-            <TbWorld className="size-5" />
+            <MdOutlineMail className="size-5" />
             <h6>jose.colcha@outlook.es</h6>
           </div>
         </div>
